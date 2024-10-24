3002a1d53dbe17de481caa93bade45b992b3b819
Daniel Johansson <dannesnake@gmail.com>
2024-10-24 17:28:29 +0200

    Call env variable directly in program.cs

--- DriverRegisterSystem/Program.cs
+++ DriverRegisterSystem/Program.cs
@@ -14,10 +14,12 @@ namespace DriverRegisterSystem
             var builder = WebApplication.CreateBuilder(args);
 
             Env.Load();
+            builder.Configuration.AddEnvironmentVariables();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-                           options.UseSqlServer(builder.Configuration.GetConnectionString("defaultconnection")));
-
+                           options.UseSqlServer(Environment.GetEnvironmentVariable("Connectionstring")
+                               /*builder.Configuration.GetConnectionString("defaultconnection")*/));
+            
             builder.Services.AddIdentity<Employee, IdentityRole>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
