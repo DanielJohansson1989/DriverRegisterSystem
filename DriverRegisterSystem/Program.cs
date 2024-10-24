ae09a332872fc6d0bd754405c95db27a9748f493
Daniel Johansson <dannesnake@gmail.com>
2024-10-24 16:30:42 +0200

    Use env for connectionstring

--- DriverRegisterSystem/Program.cs
+++ DriverRegisterSystem/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using DriverRegisterSystem.Models;
 using DriverRegisterSystem.Services;
+using DotNetEnv;
 
 namespace DriverRegisterSystem
 {
@@ -11,6 +12,8 @@ namespace DriverRegisterSystem
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            Env.Load();
 
             builder.Services.AddDbContext<AppDbContext>(options =>
                            options.UseSqlServer(builder.Configuration.GetConnectionString("defaultconnection")));
